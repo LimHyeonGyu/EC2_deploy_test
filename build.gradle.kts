8798436f971d37fd707bc796773eee4a3c732505
LimHyeunGyu <delldro94@gmail.com>
2024-11-12 15:19:43 +0900

    CI/CD test

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.storage.CacheResetOnProcessCanceled.enabled
+
 plugins {
     kotlin("jvm") version "1.9.25"
     kotlin("plugin.spring") version "1.9.25"
@@ -30,6 +32,7 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("com.h2database:h2")
 }
 
 kotlin {
@@ -46,4 +49,8 @@ allOpen {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+tasks.jar {
+    enabled = false
 }
--- src/test/kotlin/edu/example/deploytest/DeploytestApplicationTests.kt
+++ src/test/kotlin/edu/example/deploytest/DeploytestApplicationTests.kt
@@ -2,8 +2,10 @@ package edu.example.deploytest
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.TestPropertySource
 
 @SpringBootTest
+@TestPropertySource(locations = ["classpath:application-test.properties"])
 class DeploytestApplicationTests {
 
     @Test
